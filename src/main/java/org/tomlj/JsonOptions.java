c1077c73e89ea4c4217f76fb27ed6416ced4e095
Chris Leishman <chris@leishman.org>
2022-10-09 11:02:00 +0100

    Add javadoc to JsonOptions

--- src/main/java/org/tomlj/JsonOptions.java
+++ src/main/java/org/tomlj/JsonOptions.java
@@ -14,8 +14,23 @@ package org.tomlj;
 
 import java.util.EnumSet;
 
+/**
+ * Options for serializing TOML to JSON.
+ * <p>
+ * These options are suitable for use with `toJson` methods, e.g. {@link TomlTable#toJson(JsonOptions...)},
+ * {@link TomlArray#toJson(JsonOptions...)}.
+ */
 public enum JsonOptions {
-  VALUES_AS_OBJECTS_WITH_TYPE, ALL_VALUES_AS_STRINGS;
+  /**
+   * Output values as objects including a "type" and "value" property. E.g.:
+   * <p>
+   * {@code { "type": "string", "value": "hello world" }}
+   */
+  VALUES_AS_OBJECTS_WITH_TYPE,
+  /**
+   * Output all values as strings, rather than using integer, float or boolean values.
+   */
+  ALL_VALUES_AS_STRINGS;
 
   static EnumSet<JsonOptions> setFrom(JsonOptions[] options) {
     return options.length > 0 ? EnumSet.of(options[0], options) : EnumSet.noneOf(JsonOptions.class);
